a9da1afc380b05c72492fa723748c47330a5b09c
Jay Upadhyay <52772566+JayUpadhyay-8@users.noreply.github.com>
2024-05-01 22:59:07 -0400

    Update chart-script.js

--- Fire/chart-script.js
+++ Fire/chart-script.js
@@ -41,6 +41,21 @@ function createChart(ctx, title, labels, data) {
     });
 }
 
+function setupQuiz() {
+    const quiz = document.getElementById('quiz');
+    quiz.innerHTML = '<p>What should you do if your clothes catch fire?</p>' +
+                     '<input type="radio" id="stop" name="fire" value="stop"><label for="stop">Stop, Drop, and Roll</label><br>' +
+                     '<input type="radio" id="run" name="fire" value="run"><label for="run">Run</label><br>';
+}
+
+function submitQuiz() {
+    const answer = document.querySelector('input[name="fire"]:checked').value;
+    alert(answer === "stop" ? "Correct!" : "Oops! The right answer is Stop, Drop, and Roll.");
+}
+
+
+
+
 // Main function to render charts
 async function renderCharts() {
     const incidentTypeSql = `SELECT * from "91a38b1f-8439-46df-ba47-a30c48845e06" WHERE "incident_description" IS NOT NULL`;
@@ -57,6 +72,21 @@ async function renderCharts() {
     const districtCounts = processData(districtRecords, 'district');
     const districtCtx = document.getElementById('districtChart').getContext('2d');
     createChart(districtCtx, 'Incidents by District', Object.keys(districtCounts), Object.values(districtCounts));
+
+    const geojson = L.geoJson(geojsonFeature, {
+    style: function (feature) {
+        return {
+            fillColor: getColor(feature.properties.incidents),
+            weight: 2,
+            opacity: 1,
+            color: 'white',
+            dashArray: '3',
+            fillOpacity: 0.7
+        };
+    }
+}).addTo(map);
 }
 
 renderCharts();
+
+document.onload = setupQuiz();
