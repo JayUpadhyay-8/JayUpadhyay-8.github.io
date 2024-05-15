61ab36d5ed5de4a34a054cd73f7d72499f9fde11
Jay Upadhyay <52772566+JayUpadhyay-8@users.noreply.github.com>
2024-05-15 01:34:17 -0400

    Update chart-script.js

--- Fire/chart-script.js
+++ Fire/chart-script.js
@@ -41,29 +41,6 @@ function createBarChart(ctx, title, labels, data) {
     });
 }
 
-// Function to create a radar chart
-function createRadarChart(ctx, title, labels, data) {
-    new Chart(ctx, {
-        type: 'radar',
-        data: {
-            labels: labels,
-            datasets: [{
-                label: title,
-                data: data,
-                backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                borderColor: 'rgba(54, 162, 235, 1)',
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                r: {
-                    beginAtZero: true
-                }
-            }
-        }
-    });
-}
 
 // Function to setup the quiz
 function setupQuiz() {
