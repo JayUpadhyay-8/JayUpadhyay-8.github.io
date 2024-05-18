9fbfa0f22787c91336d501872bf9adc766122afe
Jay Upadhyay <52772566+JayUpadhyay-8@users.noreply.github.com>
2024-05-18 15:09:13 -0400

    Update chart-script.js

--- Fire/chart-script.js
+++ Fire/chart-script.js
@@ -173,23 +173,7 @@ function submitQuiz() {
     document.getElementById('quiz-result').textContent = resultMessage;
 }
 
-$(function() {
-  $( "#button" ).click(function() {
-    $( "#button" ).addClass( "onclic", 250, validate);
-  });
 
-  function validate() {
-    setTimeout(function() {
-      $( "#button" ).removeClass( "onclic" );
-      $( "#button" ).addClass( "validate", 450, callback );
-    }, 2250 );
-  }
-    function callback() {
-      setTimeout(function() {
-        $( "#button" ).removeClass( "validate" );
-      }, 1250 );
-    }
-  });
 
 // Main function to render charts
 async function renderCharts() {
@@ -241,3 +225,22 @@ async function renderCharts() {
 }
 
 renderCharts();
+
+
+$(function() {
+  $( "#button" ).click(function() {
+    $( "#button" ).addClass( "onclic", 250, validate);
+  });
+
+  function validate() {
+    setTimeout(function() {
+      $( "#button" ).removeClass( "onclic" );
+      $( "#button" ).addClass( "validate", 450, callback );
+    }, 2250 );
+  }
+    function callback() {
+      setTimeout(function() {
+        $( "#button" ).removeClass( "validate" );
+      }, 1250 );
+    }
+  });
